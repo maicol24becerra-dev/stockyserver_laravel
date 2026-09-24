9e403e97f91f3c840f2d9e41737bf633d168de9b
Maicol Alexis <maicol24becerra@gmail.com>
2026-09-24 08:59:11 -0500

    feat(mesero): actualizar dashboard interactivo para toma de comandas y control de pedidos

--- resources/views/mesero/dashboard.blade.php
+++ resources/views/mesero/dashboard.blade.php
@@ -12,6 +12,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ ucfirst($section === 'crear' ? 'Crear pedido' : ($section === 'historial' ? 'Historial' : 'Pedidos activos')) }} - El Cielo</title>
+    <link rel="icon" href="{{ asset('images/logo-circle.png') }}?v={{ time() }}" type="image/png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
@@ -24,7 +25,7 @@
     {{-- SIDEBAR --}}
     <aside class="sidebar">
         <div class="brand">
-            <img src="{{ asset('elcielo/assets/img/hero.png') }}?v={{ time() }}" alt="El Cielo" class="brand-logo-img">
+            <img src="{{ asset('images/logo-circle.png') }}?v={{ time() }}" alt="El Cielo" class="brand-logo-img">
             <strong>El Cielo</strong>
             <span>CENTRO VACACIONAL<br>Y RECREACIONAL</span>
         </div>
